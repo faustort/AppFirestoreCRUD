47bdb369ff8368397a3c96a5f197773d5e312697
Fausto Toloi <faustortoloi@gmail.com>
2023-04-04 15:25:48 -0300

    database firestore

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyAL8e7CY_dWHdbc_7bG86oK_IT-tYA5ZI8",
+    authDomain: "aulafirebase-9613a.firebaseapp.com",
+    projectId: "aulafirebase-9613a",
+    storageBucket: "aulafirebase-9613a.appspot.com",
+    messagingSenderId: "858458664182",
+    appId: "1:858458664182:web:1e8cde8dc062acea3710d7"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app)
+
+export default { app, auth, db };
