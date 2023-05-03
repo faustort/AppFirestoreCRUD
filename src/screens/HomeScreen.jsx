d254035977e5c220b95749a67b216dd83c9ece6b
Fausto <fausto.toloi@prof.sc.senac.br>
2023-05-03 16:14:45 -0300

    Saudacao

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,18 +1,27 @@
 import { View } from "react-native"
 import { Text } from "react-native-paper"
 import styles from "../config/styles"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { onAuthStateChanged } from "firebase/auth"
-import { auth } from "../config/firebase"
+import { auth, db } from "../config/firebase"
+import { collection, getDocs, query, where } from "firebase/firestore"
 
 export default function HomeScreen() {
+    const [usuario, setUsuario] = useState({
+        nome: "",
+        email: "",
+        telefone: "",
+        uid: ""
+    })
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
-                console.log("Usuário logado: ", user)
                 console.log("Usuário UID: ", user.uid)
-
+                setUsuario({
+                    ...usuario,
+                    uid: user.uid,
+                })
             } else {
                 console.log("Usuário não logado")
             }
@@ -21,11 +30,38 @@ export default function HomeScreen() {
     }, [])
 
 
+    useEffect(() => {
+        if (usuario?.uid === "") return
+
+        // Seleciono a coleção de usuários
+        const usersRef = collection(db, "usuarios");
+
+        // crio a query para buscar o usuário
+        const q = query(usersRef, where("userUID", "==", usuario.uid));
+        const querySnapshot = getDocs(q)
+            .then((querySnapshot) => {
+                if (!querySnapshot.empty) {
+                    const userDoc = querySnapshot.docs[0];
+                    const userData = userDoc.data();
+                    setUsuario({
+                        ...usuario,
+                        nome: userData.nome,
+                        email: userData.email,
+                        telefone: userData.telefone
+
+                    })
+                } else {
+                    console.log("User not found");
+                    return null;
+                }
+            })
+    }, [usuario?.uid])
+
 
 
     return (
         <View style={styles.container}>
-            <Text>Home Screen</Text>
+            <Text>Olá {usuario.nome}</Text>
         </View>
     )
 }
